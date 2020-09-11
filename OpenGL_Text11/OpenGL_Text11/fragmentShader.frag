ff44bb162a54797220bdf697376fe647379f3def
Felix <felix2000@foxmail.com>
2020-09-12 01:47:11 +0800

    update

--- OpenGL_Text11/OpenGL_Text11/fragmentShader.frag
+++ OpenGL_Text11/OpenGL_Text11/fragmentShader.frag
@@ -10,5 +10,5 @@ uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5)/*vec4(ourColor, 1.0)*/;
 }
--- OpenGL_Text11/OpenGL_Text11/program.cpp
+++ OpenGL_Text11/OpenGL_Text11/program.cpp
@@ -114,7 +114,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	//set texture filering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	//load image,create texture and generate mipmaps
 	data = stbi_load("lwh&ljz.jpg", &width, &height, &nrChannels, 0);
